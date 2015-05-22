3cb99881543ab37f6fcb2f1ab0ca0b0772d2d5b5
Romina Racca <romina.racca@gmail.com>
2015-05-22 13:57:26 -0300

    WS de paises

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var fs      = require('fs');
 var server = restify.createServer();
 
 //reading config file
-var config = JSON.parse(fs.readFileSync('/etc/nodejs-config/GeoObject_properties.json'));
+var config = JSON.parse(fs.readFileSync('/etc/nodejs-config/GeoObject.json'));
 
 // connection string to GeoObject database
 var conString = "postgres://" + 
@@ -103,6 +103,169 @@ server.get(
       });
    });
 });
+
+
+/*** Paises ***/
+server.get(
+   {path: '/countries', version:'1.0.0'}, 
+   function(req,res){
+      pg.connect(conString, function(err, client, done){
+      //Return if an error occurs
+      if(err) {
+         //TODO respond with error code
+         console.error('error fetching client from pool', err);
+      }
+      //querying database
+      var sql = 'SELECT id, code_iso_alfa2, code_iso_alfa3, code_iso_num, name_iso, common_name, comment, citizenship, entity, entity_code_iso_alfa2 FROM geo_object.country WHERE erased=false';
+      var responseArray = [];
+      client.query(sql, function(err, result) {
+         //Return if an error occurs
+         if(err) {
+            console.error('error fetching client from pool', err);      
+         }
+         // Storing result in an array
+         result.rows.forEach(
+            function(data) {
+               var dto = {
+                  id: data.id,
+                  code_iso_alfa2: data.code_iso_alfa2,
+                  code_iso_alfa3: data.code_iso_alfa3,
+                  code_iso_num: data.code_iso_num,
+                  name_iso: data.name_iso,
+                  common_name: data.common_name,
+                  comment: data.comment,
+                  citizenship: data.citizenship,
+                  entity: data.entity,
+                  entity_code_iso_alfa2: data.entity_code_iso_alfa2,
+                  _links: {
+                     country: {
+                        href: 'http://localhost:' + config.port + "/countries/" + data.code_iso_alfa3,
+                        type: 'application/json'
+                     }
+                  }
+               };
+               responseArray.push(dto);
+            }
+         );
+         done(); //release the pg client back to the pool 
+         var cosa = {
+            "org.geoobject.model.Country": responseArray
+         };
+         res.json(cosa);
+      });
+   });
+});
+
+
+
+server.get(
+   {path: '/countries/:code_iso_alfa3', version:'1.0.0'}, 
+   function(req,res){
+      pg.connect(conString, function(err, client, done){
+      //Return if an error occurs
+      if(err) {
+         //TODO respond with error code
+         console.error('error fetching client from pool', err);
+      }
+      //querying database
+      var sql = 'SELECT id, code_iso_alfa2, code_iso_alfa3, code_iso_num, name_iso, common_name, comment, citizenship, entity, entity_code_iso_alfa2 FROM geo_object.country WHERE erased=false AND code_iso_alfa3 ilike ';
+      sql += "'" + req.params.code_iso_alfa3 + "'";
+      console.log(sql);
+      client.query(sql, function(err, result) {
+         //Return if an error occurs
+         if(err) {
+            console.error('error fetching client from pool', err);      
+         }
+          if(!result.rows[0]) 
+            res.send(404);
+         else{
+            var dto = {
+            	id: result.rows[0].id,
+               	code_iso_alfa2: result.rows[0].code_iso_alfa2,
+               	code_iso_alfa3: result.rows[0].code_iso_alfa3,
+               	code_iso_num: result.rows[0].code_iso_num,
+               	name_iso: result.rows[0].name_iso,
+               	common_name: result.rows[0].common_name,
+               	comment: result.rows[0].comment,
+               	citizenship: result.rows[0].citizenship,
+               	entity: result.rows[0].entity,
+               	entity_code_iso_alfa2: result.rows[0].entity_code_iso_alfa2,
+               	_links: {
+                	country: {
+                    rel : 'self',
+                    href: 'http://localhost:' + config.port + "/countries/" + result.rows[0].code_iso_alfa3,
+                    type: 'application/json'
+                  }
+               }
+            };
+            var cosa = {
+               "org.geoobject.model.Country" : dto
+            }
+            res.json(cosa);
+         }
+         done(); //release the pg client back to the pool 
+      });
+   });
+});
+
+
+
+server.get(
+   {path: '/continents/:code/countries', version:'1.0.0'}, 
+   function(req,res){
+      pg.connect(conString, function(err, client, done){
+      //Return if an error occurs
+      if(err) {
+         //TODO respond with error code
+         console.error('error fetching client from pool', err);
+      }
+      //querying database
+      var sql = 'SELECT country.id, country.code_iso_alfa2, country.code_iso_alfa3, country.code_iso_num, country.name_iso, country.common_name, country.comment, country.citizenship, country.entity, country.entity_code_iso_alfa2, continent.name FROM geo_object.country country LEFT JOIN geo_object.continent continent ON country.continent_id = continent.id WHERE country.erased=false AND continent.code ilike ';
+   		sql += "'" + req.params.code + "'";
+      var responseArray = [];
+      client.query(sql, function(err, result) {
+         //Return if an error occurs
+         if(err) {
+            console.error('error fetching client from pool', err);      
+         }
+         // Storing result in an array
+         result.rows.forEach(
+            function(data) {
+               var dto = {
+                  id: data.id,
+                  code_iso_alfa2: data.code_iso_alfa2,
+                  code_iso_alfa3: data.code_iso_alfa3,
+                  code_iso_num: data.code_iso_num,
+                  name_iso: data.name_iso,
+                  common_name: data.common_name,
+                  comment: data.comment,
+                  citizenship: data.citizenship,
+                  entity: data.entity,
+                  entity_code_iso_alfa2: data.entity_code_iso_alfa2,
+                  name: data.name,
+                  _links: {
+                     continent: {
+                     	href: 'http://localhost:' + config.port + "/continents/" + req.params.code,
+                        type: 'application/json'
+                     },
+                     country: {
+                        href: 'http://localhost:' + config.port + "/countries/" + data.code_iso_alfa3,
+                        type: 'application/json'
+                     }
+                  }
+               };
+               responseArray.push(dto);
+            }
+         );
+         done(); //release the pg client back to the pool 
+         var cosa = {
+            "org.geoobject.model.Country": responseArray
+         };
+         res.json(cosa);
+      });
+   });
+});
+
 
 server.listen(config.port);
 console.log("GeoObject Listening on port " + config.port);
